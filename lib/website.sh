5e96c53cea2e789d351c41033269b67d9d22d555
dorofeev200 <dorofeevigor20@gmail.com>
2026-04-11 14:12:26 +0300

    website.sh

---
+++ lib/website.sh
@@ -0,0 +1,12 @@
+website_menu() {
+    clear
+    echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+    echo "       САЙТ / ШАБЛОНЫ"
+    echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+    echo
+    echo "1) Lite"
+    echo "2) Pro"
+    echo
+    read -p "Enter..."
+    show_main
+}
