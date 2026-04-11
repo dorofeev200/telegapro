bc3f379523b20f4f946b7d855b31fb7e84ed0a18
dorofeev200 <dorofeevigor20@gmail.com>
2026-04-11 14:11:46 +0300

    stats.sh

--- lib/stats.sh
+++ lib/stats.sh
@@ -1,1 +1,12 @@
-
+stats_menu() {
+    clear
+    echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+    echo "        DASHBOARD — TelegaPro"
+    echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+    echo
+    echo "Прокси: running"
+    echo "nginx: running"
+    echo
+    read -p "Enter..."
+    show_main
+}
