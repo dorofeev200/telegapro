1de1168ddb1eb5d6ba8c02207de1881c06e24f67
dorofeev200 <dorofeevigor20@gmail.com>
2026-04-11 14:12:54 +0300

    bot.sh

---
+++ lib/bot.sh
@@ -0,0 +1,11 @@
+bot_menu() {
+    clear
+    echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+    echo "       TELEGRAM-БОТ"
+    echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+    echo
+    echo "Ваш бот готов"
+    echo
+    read -p "Enter..."
+    show_main
+}
